52a441938cf7df8247c860caeb139fb207f01d16
rie03p <hgrt.0822@gmail.com>
2025-12-14 07:10:33 +0000

    feat: add xo

---
+++ xo.config.js
@@ -0,0 +1,20 @@
+/** @type {import('xo').Options} */
+export default {
+  space: 2,
+
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'capitalized-comments': 'off',
+    'import-x/extensions': 'off',
+    'n/file-extension-in-import': 'off',
+    'new-cap': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+  },
+
+  ignores: [
+    'node_modules',
+    '.next',
+    'out',
+    'public',
+  ],
+}
